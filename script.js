763c648831925089e9ed5477dcbefcac0728edfa
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-08-13 13:19:30 +0530

    analytics

---
+++ script.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+   script.defer = true;
+   script.src = 'https://cdn.cookie3.co/scripts/analytics/0.11.4/cookie3.analytics.min.js';
+   script.dataset.siteId = "1392";
+   document.getElementsByTagName('head')[0].appendChild(script);
