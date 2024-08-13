c2f2ab4982bff88a56887cf959d705d18d96847e
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-08-13 13:23:35 +0530

    analytics

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var script = document.createElement('script');
-   script.defer = true;
-   script.src = 'https://cdn.cookie3.co/scripts/analytics/0.11.4/cookie3.analytics.min.js';
-   script.dataset.siteId = "1392";
-   document.getElementsByTagName('head')[0].appendChild(script);
+<script
+   src="https://cdn.cookie3.co/scripts/analytics/0.11.4/cookie3.analytics.min.js"
+   integrity="sha384-lzDmDdr/zEhMdlE+N04MgISCyL3RIWNCb9LjsrQeEFi8Gy5CKXIRI+u58ZV+ybYz"
+   crossOrigin="anonymous"
+   async="true"
+   strategy="lazyOnload"
+   data-site-id="1392"
+   data-chain-tracking-enabled="true"
+></script>
